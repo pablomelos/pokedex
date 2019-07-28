56539983f711e4dc14095dac4b38c089d9ea44e1
pablomelos <51058128+pablomelos@users.noreply.github.com>
2019-07-28 16:52:20 -0300

    pokedex.ccp
    Pokedex comision k 1094 algoritmos y e. datos. 
    lo estoy trabajando en la maquina con el dev de c++ y lo voy actualizando aca.

--- test.cpp
+++ test.cpp
@@ -1,46 +1,93 @@
 #include <iostream>
 using namespace std;
 
- 
-int main()
+//declaracion de funciones
+//string nomb_poke(); no me zalee es inpozzibleee!
+int nivel_poke();
+string tipo_poke();
 
-{
-
+//declaracion de variables
 string nombre, tipo;
 int nivel;
 string nom[100];
 string tip[100];
 int nivell[100];
 int e=0;
-nombre = "si";
+string consulta="";
 
+
+int main()
+
+{
 	cout << "*POKEDEX*"<< endl;
 	cout << "*-*-*-*-*-*-*"<< endl;
 	
-	cout << "INGRESE NOMBRE DE SU POKEMON (ingreso no para finalizar): "<< endl;
-	cin >> nombre;
-	while (nombre != "no") {
-	cout << "INGRESE NIVEL :"<< endl;
-	cin >> nivel;
-
-	cout << "INGRESE TIPO (F/A/T/E)"<< endl;
-	cin >> tipo;
+	while (consulta != "no") {
 	
-	cout << "INGRESE NOMBRE DE SU POKEMON (ingreso no para finalizar): "<< endl;
-	cin >> nombre;
+		cout << "INGRESE NOMBRE DE SU POKEMON : "<< endl;
+		cin >> nombre;
+		//nomb_poke();
+			
+		cout << "INGRESE NIVEL :"<< endl;
+		nivel=nivel_poke();
+		
+		cout << "INGRESE TIPO (F/A/T/E)"<< endl;
+		tipo=tipo_poke();
+
+		cout << "DESEA CARGAR OTRO POKEMON (si/no): "<< endl;
+		cin >> consulta;
 	
 	nom[e]=nombre;
 	tip[e]=tipo;
 	nivell[e]=nivel;
+	
 	e++;
-	
 	}
-	
+	// control de carga para verificar funcionamiento del array. 
+	//No es necesario para el funcionamiento del software.	
 	int i;
-	for (i=0;i<= e; i++) {
+	for (i=0;i< e; i++) {
 	cout << nom[i];
+	cout << "  " << endl;
 	cout << tip[i];
+	cout << "  " << endl;
 	cout << nivell[i];
+	cout << "  " << endl;
 	}
+	// fin de control. 
+	
 	return 0;
 }
+
+//string nomb_poke()
+//	{
+//		cin >> nombre;
+//		while (sizeof(nombre) > 1000){
+//			cout << "nombre no valido ingres nuevamente su pokemon: " << endl;
+//			cin >> nombre;
+//		}
+//		return nombre;
+//	}
+	
+	
+int nivel_poke()
+	{
+		cin >> nivel;
+		while (nivel >1000){
+			cout << "Nivel fuera de rango, ingrese nuevamente un numero: ";
+			cin >> nivel;
+		}
+		return nivel;
+	}
+	
+string tipo_poke(){
+	string a,f,t,e;
+	cin >> tipo;
+	while (tipo != "f"){
+		cout << " valor incorrecto, elija el tipo con una letra (t/f/a/e): ";
+		cin >> tipo;
+	}
+	return tipo;
+}
+		
+
